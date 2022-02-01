a7f268599e523509f87dcbfe5fdd7c3d6c594ba5
Sarah <sarahbezerra2013@gmail.com>
2022-02-01 20:45:55 -0300

    Layout Tela de Cadastro

--- src/components/RegistrationPage/index.js
+++ src/components/RegistrationPage/index.js
@@ -1,5 +1,24 @@
+import React, { useState } from "react"
+import axios from "axios";
+import { StyledAppName } from "../../style/StyledAppName";
+import { StyledForm } from '../../style/StyledForm';
+import { StyledLink } from "../../style/StyledLink";
+
 export default function RegistrationPage(){
+
     return(
-        <h1>RegistrationPage</h1>
+        <>
+        <StyledAppName>MyWallet</StyledAppName>
+        <StyledForm>
+            <fieldset>
+                <input type="name" placeholder="Nome" name="name" required></input>
+                <input type="email" placeholder="E-mail" name="email" required></input>
+                <input type="password" placeholder="Senha" name="password" required></input>
+                <input type="confirm_password" placeholder="Confirme a senha" name="confirm_password" required></input>
+                <button type="submit">Cadastrar</button>
+            </fieldset>
+        </StyledForm>
+        <StyledLink to="/">Já tem uma conta? Entre agora!</StyledLink>
+        </>
     )
 }
