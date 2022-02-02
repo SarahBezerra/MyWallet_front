6c162d356f0b5f5e900915f6eb18962355b091ed
Sarah <sarahbezerra2013@gmail.com>
2022-02-02 14:05:47 -0300

    Envia dados de cadastro para o servidor

--- src/components/RegistrationPage/index.js
+++ src/components/RegistrationPage/index.js
@@ -1,21 +1,66 @@
 import React, { useState } from "react"
-import axios from "axios";
-import { StyledAppName } from "../../style/StyledAppName";
-import { StyledForm } from '../../style/StyledForm';
-import { StyledLink } from "../../style/StyledLink";
+import axios from "axios"
+import { ThreeDots } from 'react-loader-spinner'
+import { useNavigate } from "react-router"
+
+import { StyledAppName } from "../../style/StyledAppName"
+import { StyledForm } from '../../style/StyledForm'
+import { StyledLink } from "../../style/StyledLink"
 
 export default function RegistrationPage(){
+
+    const navigate = useNavigate();
+    const [isEnabled, setIsEnabled] = useState(true);
+    const [registrationData, setRegistrationData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        confirm_password: ""
+    });
+    
+    function handleInputChange(e){
+        setRegistrationData({...registrationData, [e.target.name]: e.target.value})
+        console.log(registrationData)
+    }
+    
+    function handleRegistration(e){
+        e.preventDefault();
+        setIsEnabled(false);
+
+        if(e.target.password.value !== e.target.confirm_password.value){
+            alert("Confira se os dados foram informados corretamente")
+            setIsEnabled(true)
+            return
+        }
+
+        const promise = axios.post("http://localhost:5000/cadastro", {
+            name: registrationData.name,
+            email: registrationData.email,
+            password: registrationData.password
+        });
+        
+        promise.then(response => {
+            setIsEnabled(true)
+            console.log(response.data)
+            navigate("/")
+        })
+        promise.catch(error => {
+            setIsEnabled(true)
+            alert(error.response.data)
+        })
+    }
+
 
     return(
         <>
         <StyledAppName>MyWallet</StyledAppName>
-        <StyledForm>
-            <fieldset>
-                <input type="name" placeholder="Nome" name="name" required></input>
-                <input type="email" placeholder="E-mail" name="email" required></input>
-                <input type="password" placeholder="Senha" name="password" required></input>
-                <input type="confirm_password" placeholder="Confirme a senha" name="confirm_password" required></input>
-                <button type="submit">Cadastrar</button>
+        <StyledForm onSubmit={handleRegistration} enabled={isEnabled}>
+            <fieldset disabled={!isEnabled}>
+                <input type="name" placeholder="Nome" name="name" value={registrationData.name} onChange={handleInputChange} required></input>
+                <input type="email" placeholder="E-mail" name="email" value={registrationData.email} onChange={handleInputChange} required></input>
+                <input type="password" placeholder="Senha" name="password" value={registrationData.password} onChange={handleInputChange} required></input>
+                <input type="password" placeholder="Confirme a senha" name="confirm_password" value={registrationData.confirm_password} onChange={handleInputChange} required></input>
+                <button type="submit">{isEnabled ? "Cadastrar" : <ThreeDots type="ThreeDots" color="#FFF" height={13} width={38} />}</button>
             </fieldset>
         </StyledForm>
         <StyledLink to="/">Já tem uma conta? Entre agora!</StyledLink>
--- src/style/StyledForm.js
+++ src/style/StyledForm.js
@@ -12,10 +12,15 @@ export const StyledForm = styled.form`
             height: 58px;
             margin-bottom: 13px;
 
-            background-color: #FFFFFF;
+            background-color: ${(props) => props.enabled ? "#FFFFFF" : "#F0F8FF"};
             border-radius: 5px;
             border: none;
             padding: 15px;
+
+            color: ${(props) => props.enabled ? "#000" : "#808080"};
+            font-weight: 400;
+            font-size: 20px;
+            line-height: 23px;
 
             ::placeholder{
                 font-weight: 400;
@@ -26,12 +31,16 @@ export const StyledForm = styled.form`
         }
 
         button{
+            display: flex;
+            justify-content: center;
+            align-items: center;
             width: 326px;
             height: 46px;
 
             background-color: #A328D6;
             border-radius: 5px;
             border: none;
+            opacity: ${(props) => props.enabled ? 1 : 0.7};
 
             font-weight: 700;
             font-size: 20px;
